8ef921b290e578bb83cd16a62fa70fc3ed0f88dd
Luiz-Felipe-Magalhaes <luizfelipe.magalhaes@outlook.com>
2021-01-25 22:42:50 -0300

    criação de tabela Temas no db, e relacionamento com tabela Postagens.

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table (name = "postagem")
@@ -31,6 +34,10 @@ public class Postagem
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 	
 	//Getters and Setters:
 	public long getId() {
@@ -56,7 +63,15 @@ public class Postagem
 	}
 	public void setData(Date data) {
 		this.data = data;
+	}
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}	
+	
+	
 	
 	
 	
