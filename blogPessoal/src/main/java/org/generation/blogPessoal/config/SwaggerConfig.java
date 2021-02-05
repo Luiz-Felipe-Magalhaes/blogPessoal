cb4e26491218f404b60287d17480d4c4c1815efb
Luiz-Felipe-Magalhaes <luizfelipe.magalhaes@outlook.com>
2021-02-05 15:41:45 -0300

    Criação do deploy e documentação automática via Swagger.

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package org.generation.blogPessoal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("org.generation.blogPessoal.controller"))
+                .paths(PathSelectors.any()).build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Blog Pessoal - Luiz Felipe Magalhães").description("API do Blog pessoal Generation - Swagger2").version("1.0")
+                .contact(contact()).build();
+    }
+
+    private Contact contact() {
+        return new Contact("Luiz Felipe Magalhães", "https://github.com/Luiz-Felipe-Magalhaes", "Desenvolvedor Java Full Stack");
+    }
+}
